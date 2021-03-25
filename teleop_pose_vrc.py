0fd0947d98c42c0d218e2d2a4262235108bfeab1
Kuwamai <mitsumex333@gmail.com>
2021-03-26 01:45:49 +0900

    Add: オブジェクト位置情報の展開

--- teleop_pose_vrc.py
+++ teleop_pose_vrc.py
@@ -1,13 +1,34 @@
 import cv2
+import time
+import numpy as np
 
-capture = cv2.VideoCapture(1)
+def unpack_tex(frame):
+    block = frame.shape[0]/16
+    ix = int(block / 2)
+    frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    th, frame_th = cv2.threshold(frame_rgb, 128, 1, cv2.THRESH_BINARY)
+    pos = np.array([0, 0, 0])
+    for i in range(16):
+        iy = int(block / 2 + block * i)
+        print(frame_th[iy, ix])
+        pos += frame_th[iy, ix] * 2 ** (15 - i)
+    pos = (pos - 32768) / 1000
+    print(pos)
 
-while(True):
-    ret, frame = capture.read()
-    frame = cv2.resize(frame, (frame.shape[1], frame.shape[0]))
-    cv2.imshow('title',frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+def main():
+    capture = cv2.VideoCapture(1)
+    while(True):
+        ret, frame = capture.read()
+        unpack_tex(frame)
+        frame = cv2.resize(frame, (frame.shape[1], frame.shape[0]))
+        cv2.imshow('title',frame)
+        time.sleep(0.05)
 
-capture.release()
-cv2.destroyAllWindows()
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    capture.release()
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    main()
