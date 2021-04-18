beebf35aa31e6861e05a3045d6af7868c7d64a31
Kuwamai <mitsumex333@gmail.com>
2021-04-18 23:40:41 +0900

    feat: poseをpublish

--- teleop_pose_vrc.py
+++ teleop_pose_vrc.py
@@ -1,32 +1,59 @@
 import cv2
 import time
 import numpy as np
+import sys
+import roslibpy
 
 def unpack_tex(frame):
     block = frame.shape[0]/16
     ix = int(block / 2)
-    frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    frame_rgb = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
     th, frame_th = cv2.threshold(frame_rgb, 128, 1, cv2.THRESH_BINARY)
     pos = np.array([0, 0, 0])
     for i in range(16):
         iy = int(block / 2 + block * i)
-        print(frame_th[iy, ix])
         pos += frame_th[iy, ix] * 2 ** (15 - i)
     pos = (pos - 32768) / 1000
     print(pos)
+    return pos
 
 def main():
+    ip_address = sys.argv[1]
+    client = roslibpy.Ros(ip_address, 9090)
+    client.run()
+
+    topic_name = "/arm_pose"
+    message_type = "geometry_msgs/Pose"
+    talker = roslibpy.Topic(client, topic_name, message_type)
+
     capture = cv2.VideoCapture(1)
     while(True):
         ret, frame = capture.read()
-        unpack_tex(frame)
+        pos = unpack_tex(frame)
+        talker.publish(roslibpy.Message({
+            "position": {
+                "x": pos[0],
+                "y": pos[1],
+                "z": pos[2]
+            },
+            "orientation": {
+                "x": 0,
+                "y": 0,
+                "z": 0,
+                "w": 1
+            }
+        }))
+
         frame = cv2.resize(frame, (frame.shape[1], frame.shape[0]))
         cv2.imshow('title',frame)
-        time.sleep(0.05)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
+        time.sleep(0.05)
+
+    talker.unadvertise()
+    client.terminate()
     capture.release()
     cv2.destroyAllWindows()
 
